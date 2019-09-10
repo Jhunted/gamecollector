977d2818cf6dade9c4988fcb3b3ef4ae51e986a9
Joseph Hunt <jhunted54@gmail.com>
2019-09-09 20:12:30 -0500

    adds refactored code and correct routing

--- main_app/views.py
+++ main_app/views.py
@@ -23,6 +23,7 @@ def about(request):
     return render(request, 'about.html')
 
 def games_index(request):
+    games = Game.objects.all()
     return render(request, 'games/index.html', {'games': games})
 
 def games_detail(request, game_id):
