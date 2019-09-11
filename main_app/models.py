16103bb3ebbfbacd584f1e21331c83daab20d55b
Joseph Hunt <jhunted54@gmail.com>
2019-09-10 20:42:23 -0500

    adds model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+PLAYDAYS = (
+  ('S', 'Sunday'),
+  ('M', 'Monday'),
+  ('T', 'Tuesday'),
+  ('W', 'Wednesday'),
+  ('TR', 'Thursday'),
+  ('F', 'Friday'),
+  ('SAT', 'Saturday'),
+)
 
 class Game(models.Model):
     name = models.CharField(max_length=100)
@@ -8,5 +18,20 @@ class Game(models.Model):
     description = models.TextField(max_length=250)
     rating = models.IntegerField()
 
-    def __str__(self):
-        return f'{self.name} ({self.id})'
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'game_id': self.id})
+
+class Session(models.Model):
+  date = models.DateField('session date')
+  session = models.CharField(
+    max_length=1,
+    choices=PLAYDAYS,
+    default=PLAYDAYS[0][0]
+  )
+  game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return "{self.get_session_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
