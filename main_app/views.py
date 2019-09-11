9bcf80b85dfd9142e177becc82488df639c254bc
Joseph Hunt <jhunted54@gmail.com>
2019-09-11 09:01:49 -0500

    adds views

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Game
+from .forms import SessionForm
 
-# from django.http import HttpResponse
 
-# class Game:  # Note that parens are optional if not inheriting from another class
-#   def __init__(self, name, genre, description, rating):
-#     self.name = name
-#     self.genre = genre
-#     self.description = description
-#     self.rating = rating
 
-# games = [
-#   Game('Apex Legends', 'shooter', 'battle-royale', 5),
-#   Game('Halo 3', 'shooter', 'first-person shooter', 5),
-#   Game('Call of Duty', 'shooter', 'first-person shooter', 2)
-# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -28,4 +18,27 @@ def games_index(request):
 
 def games_detail(request, game_id):
     game = Game.objects.get(id=game_id)
-    return render(request, 'games/detail.html', {'game': game})
+    session_form = SessionForm()
+    return render(request, 'games/detail.html',
+    { 'game': game, 'session_form': session_form })
+
+class GameCreate(CreateView):
+  model = Game
+  fields = '__all__'
+
+class GameUpdate(UpdateView):
+  model = Game
+  fields = ['genre', 'description', 'rating']
+
+class GameDelete(DeleteView):
+  model = Game
+  success_url = '/games/'
+
+def add_session(request, game_id):
+  form = SessionForm(request.POST)
+  if form.is_valid():
+    new_session = form.save(commit=False)
+    new_session.game_id = game_id
+    new_session.save()
+  return redirect('detail', game_id=game_id)
+
