7ace7b9aca0bd6a00b65ee524e414f843653fa3f
Joseph Hunt <jhunted54@gmail.com>
2019-09-12 10:49:46 -0500

    adds location and photo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 PLAYDAYS = (
@@ -12,17 +13,30 @@ PLAYDAYS = (
   ('SAT', 'Saturday'),
 )
 
+class Location(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('locations_detail', kwargs={'pk': self.id})
+
 class Game(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     rating = models.IntegerField()
+    locations = models.ManyToManyField(Location)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'game_id': self.id})
+
+    def fed_for_today(self):
+        return self.session_set.filter(date=date.today()).count() >= len(PLAYDAYS)
 
 class Session(models.Model):
   date = models.DateField('session date')
@@ -38,3 +52,10 @@ class Session(models.Model):
 
   class Meta:
     ordering = ['-date']
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  cat = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for game_id: {self.game_id} @{self.url}"
