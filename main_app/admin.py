612f5266ddac45fa8802e895c27578279f26c220
Joseph Hunt <jhunted54@gmail.com>
2019-09-12 11:05:24 -0500

    adds routes

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Game, Session
+from .models import Game, Session, Location, Photo
 
 # Register your models here.
 
 admin.site.register(Game)
 admin.site.register(Session)
+admin.site.register(Location)
+admin.site.register(Photo)
--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     path('games/<int:pk>/update/', views.GameUpdate.as_view(), name='games_update'),
     path('games/<int:pk>/delete/', views.GameDelete.as_view(), name='games_delete'),
     path('games/<int:game_id>/add_session/', views.add_session, name='add_session'),
+    path('games/<int:game_id>/assoc_location/<int:location_id>/', views.assoc_location, name='assoc_location'),
+    path('games/<int:game_id>/add_photo/', views.add_photo, name='add_photo'),
+    path('locations/', views.LocationList.as_view(), name='locations_index'),
+    path('locations/<int:pk>/', views.LocationDetail.as_view(), name='locations_detail'),
+    path('locations/create/', views.LocationCreate.as_view(), name='locations_create'),
+    path('locations/<int:pk>/update/', views.LocationUpdate.as_view(), name='locations_update'),
+    path('locations/<int:pk>/delete/', views.LocationDelete.as_view(), name='locations_delete'),
 ]
